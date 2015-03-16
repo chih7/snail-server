aeb67d530800aa74422c7de452d889dc277b4439
chih7 <i@chih.me>
2015-03-16 20:38:03 +0800

    change port to 8000

--- app/test.py
+++ app/test.py
@@ -571,7 +571,7 @@ def upload():
 #     '''
 
 
-# if __name__ == '__main__':
-#     if not os.path.exists('db.sqlite'):
-#         db.create_all()
-#     app.run(debug=True, host='0.0.0.0', port=80)
+if __name__ == '__main__':
+    if not os.path.exists('db.sqlite'):
+        db.create_all()
+    app.run(debug=True, port=8000)
